deb568bf0eab9340bdcda34b46655980e08f7751
Fery Wardiyanto <ferywardiyanto@gmail.com>
2024-12-11 20:56:49 +0700

    chore: revert 0f48ae8
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- eslint.config.js
+++ eslint.config.js
@@ -6,6 +6,5 @@ export default antfu({}, {
   rules: {
     'jsdoc/require-property-description': ['off'],
     'jsdoc/require-returns-description': ['off'],
-    'vitest/no-import-node-test': ['off'],
   },
 })
