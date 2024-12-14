0826e236ba55282c25163292f33fe207881115e4
Fery Wardiyanto <ferywardiyanto@gmail.com>
2024-12-14 18:43:43 +0700

    chore: extraxt test fixture so another test file can use it as well
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

---
+++ test/fixtures.js
@@ -0,0 +1,63 @@
+/**
+ * @typedef {object} Fixture
+ * @property {number} length
+ * @property {number} matchLen
+ * @property {number} roundLen
+ * @property {number[]} structures
+ * @property {import('../lib/parties.js').Party[]} parties
+ * @property {import('../lib/matches.js').Matchup[]} matchups
+ * @property {import('../lib/matches.js').Match[]} matches
+ * @property {import('../lib/rounds.js').Round[]} rounds
+ */
+
+/** @type {Fixture[]} */
+export const fixtures = [
+  { length: 3, roundLen: 2, structures: [1, 2] },
+  { length: 4, roundLen: 2, structures: [2, 2] },
+  { length: 5, roundLen: 3, structures: [2, 1, 2] },
+  { length: 6, roundLen: 3, structures: [] },
+  { length: 7, roundLen: 3, structures: [] },
+  { length: 8, roundLen: 3, structures: [] },
+  { length: 9, roundLen: 4, structures: [] },
+  { length: 10, roundLen: 4, structures: [] },
+  { length: 11, roundLen: 4, structures: [] },
+  { length: 12, roundLen: 4, structures: [] },
+  { length: 13, roundLen: 4, structures: [] },
+  { length: 14, roundLen: 4, structures: [] },
+  { length: 15, roundLen: 4, structures: [] },
+  { length: 16, roundLen: 4, structures: [] },
+  { length: 17, roundLen: 5, structures: [] },
+  { length: 18, roundLen: 5, structures: [] },
+  { length: 19, roundLen: 5, structures: [] },
+  { length: 20, roundLen: 5, structures: [] },
+  { length: 21, roundLen: 5, structures: [] },
+  { length: 22, roundLen: 5, structures: [] },
+  { length: 23, roundLen: 5, structures: [] },
+  { length: 24, roundLen: 5, structures: [] },
+  { length: 25, roundLen: 5, structures: [] },
+  { length: 26, roundLen: 5, structures: [] },
+  { length: 27, roundLen: 5, structures: [] },
+  { length: 28, roundLen: 5, structures: [] },
+  { length: 29, roundLen: 5, structures: [] },
+  { length: 30, roundLen: 5, structures: [] },
+  { length: 31, roundLen: 5, structures: [] },
+  { length: 32, roundLen: 5, structures: [] },
+  { length: 33, roundLen: 6, structures: [] },
+  { length: 34, roundLen: 6, structures: [] },
+  { length: 35, roundLen: 6, structures: [] },
+  { length: 36, roundLen: 6, structures: [] },
+  { length: 37, roundLen: 6, structures: [] },
+  { length: 38, roundLen: 6, structures: [] },
+  { length: 39, roundLen: 6, structures: [] },
+  { length: 40, roundLen: 6, structures: [] },
+  { length: 41, roundLen: 6, structures: [] },
+  { length: 42, roundLen: 6, structures: [] },
+  { length: 43, roundLen: 6, structures: [] },
+  { length: 44, roundLen: 6, structures: [] },
+  { length: 45, roundLen: 6, structures: [] },
+  { length: 46, roundLen: 6, structures: [] },
+  { length: 47, roundLen: 6, structures: [] },
+  { length: 48, roundLen: 6, structures: [] },
+  { length: 49, roundLen: 6, structures: [] },
+  { length: 50, roundLen: 6, structures: [] },
+]
