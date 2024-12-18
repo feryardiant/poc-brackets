321927b1882e7ce85318d6ffa3b07145b530b976
Fery Wardiyanto <ferywardiyanto@gmail.com>
2024-12-18 23:06:57 +0700

    test: add `left` and `right` arrow key event tests
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- test/browser.test.js
+++ test/browser.test.js
@@ -1,11 +1,27 @@
 'use strict'
 // @vitest-environment jsdom
 
-import { } from '@vitest/browser/context'
-import { describe, expect, it } from 'vitest'
+import { page, userEvent } from '@vitest/browser/context'
+import { beforeEach, describe, expect, it } from 'vitest'
 
 describe('interactivity', () => {
-  it('dummy', () => {
-    expect(typeof window).not.toBe('undefined')
+  beforeEach(() => {
+    userEvent.setup()
+  })
+
+  it('use right arrow to increate number of participant', async () => {
+    const $range = page.getByPlaceholder('Generate participant')
+
+    document.dispatchEvent(new KeyboardEvent('keyup', { key: 'ArrowRight' }))
+
+    await expect.element($range).toHaveValue('6')
+  })
+
+  it('use left arrow to increate number of participant', async () => {
+    const $range = page.getByPlaceholder('Generate participant')
+
+    document.dispatchEvent(new KeyboardEvent('keyup', { key: 'ArrowLeft' }))
+
+    await expect.element($range).toHaveValue('5')
   })
 })
